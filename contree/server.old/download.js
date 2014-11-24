2c1b5fdb8f8785d4cbcebaf902dfc6d5189ca2a2
Khemal <aur.vasselle@gmail.com>
2014-11-25 00:37:14 +0100

    Deplacement
    Suite ...

---
+++ contree/server.old/download.js
@@ -0,0 +1,7 @@
+﻿var http = require('http');
+var fs = require('fs');
+
+var file = fs.createWriteStream("file.png");
+var request = http.get("http://www.gravatar.com/avatar/40bec8c5a2a804cc48ab9d8443592b90?r=pg&s=200.jpg&d=identicon", function(response) {
+  response.pipe(file);
+});
