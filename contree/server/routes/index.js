a2adc5321533b7021b4ba0218cf52b11e3e256b6
Khemal <aur.vasselle@gmail.com>
2014-12-01 18:06:37 +0100

    Add .hjs layout

--- contree/server/routes/index.js
+++ contree/server/routes/index.js
@@ -4,9 +4,18 @@ var router = express.Router();
 
 //Home page
 router.get('/',function(req,res){
-	res.render('index',{
+	res.render('home',{
 		title: "Contree"
 	});
 });
+
+router.get('/login',function(req,res){
+	res.render('login');
+});
+
+router.get('/chat',function(req,res){
+	res.render('chat');
+});
+
 
 module.exports = router;
